f1abc7fe0fda0101fdf0b05d8c50ba15ec6516b5
Carmen-Git-It <carmenwhitton@gmail.com>
2022-06-26 16:16:23 -0400

    Registering slash commands works

--- slashcommands.go
+++ slashcommands.go
@@ -1,7 +1,7 @@
 package main
 
 import (
-	"log"
+	"fmt"
 
 	"github.com/bwmarrin/discordgo"
 )
@@ -19,37 +19,50 @@ var Commands = []*discordgo.ApplicationCommand{
 
 var CommandHandlers = map[string]func(dg *discordgo.Session, i *discordgo.InteractionCreate){
 	"test-command": func(dg *discordgo.Session, i *discordgo.InteractionCreate) {
-		dg.InteractionRespond(i.Interaction, &discordgo.InteractionResponse{
+		err := dg.InteractionRespond(i.Interaction, &discordgo.InteractionResponse{
 			Type: discordgo.InteractionResponseChannelMessageWithSource,
 			Data: &discordgo.InteractionResponseData{
 				Content: "This is the first tester function, good job!",
 			},
 		})
+		if err != nil {
+			fmt.Println("Error with function test-command:")
+			fmt.Println(err)
+		}
 	},
 	"test-command2": func(dg *discordgo.Session, i *discordgo.InteractionCreate) {
-		dg.InteractionRespond(i.Interaction, &discordgo.InteractionResponse{
+		err := dg.InteractionRespond(i.Interaction, &discordgo.InteractionResponse{
 			Type: discordgo.InteractionResponseChannelMessageWithSource,
 			Data: &discordgo.InteractionResponseData{
 				Content: "This is the second tester function, yay!",
 			},
 		})
+		if err != nil {
+			fmt.Println("Error with function test-command2:")
+			fmt.Println(err)
+		}
 	}}
 
 func addHandlers(dg *discordgo.Session, i *discordgo.InteractionCreate) {
 	dg.AddHandler(func(dg *discordgo.Session, i *discordgo.InteractionCreate) {
 		if handler, ok := CommandHandlers[i.ApplicationCommandData().Name]; ok {
 			handler(dg, i)
+		} else {
+			fmt.Println("Error adding handler")
 		}
 	})
 }
 
 func registerCommands(dg *discordgo.Session) {
+	fmt.Println("Registering commands...")
 	registeredCommands := make([]*discordgo.ApplicationCommand, len(Commands))
 	for i, v := range Commands {
-		command, err := dg.ApplicationCommandCreate(dg.State.User.ID, "", v)
+		command, err := dg.ApplicationCommandCreate(dg.State.User.ID, "985707181854826497", v)
 		if err != nil {
-			log.Println("Error! Cannot create command!")
+			fmt.Println("Error! Cannot create command!")
+			fmt.Println(err)
 		}
 		registeredCommands[i] = command
 	}
+
 }
