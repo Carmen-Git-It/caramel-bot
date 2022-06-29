9976599ea10fc76c5f47909dacdef7a879045995
Carmen-Git-It <carmenwhitton@gmail.com>
2022-06-29 00:13:11 -0400

    Implemented bitch slashcommand

--- slashcommands.go
+++ slashcommands.go
@@ -12,15 +12,7 @@ const testGuildID = "985707181854826497"
 
 var Commands = []*discordgo.ApplicationCommand{
 	{
-		Name:        "test-command",
-		Description: "A simple test command",
-	},
-	{
-		Name:        "test-command2",
-		Description: "A second simple test command",
-	},
-	{
-		Name:        "compliment-user",
+		Name:        "compliment",
 		Description: "Give another user a compliment",
 		Options: []*discordgo.ApplicationCommandOption{
 			{
@@ -31,34 +23,22 @@ var Commands = []*discordgo.ApplicationCommand{
 			},
 		},
 	},
+	{
+		Name:        "bitch",
+		Description: "Call another user a bitch",
+		Options: []*discordgo.ApplicationCommandOption{
+			{
+				Name:        "user",
+				Description: "The user you would like to call a bitch",
+				Type:        discordgo.ApplicationCommandOptionUser,
+				Required:    true,
+			},
+		},
+	},
 }
 
 var CommandHandlers = map[string]func(dg *discordgo.Session, i *discordgo.InteractionCreate){
-	"test-command": func(dg *discordgo.Session, i *discordgo.InteractionCreate) {
-		err := dg.InteractionRespond(i.Interaction, &discordgo.InteractionResponse{
-			Type: discordgo.InteractionResponseChannelMessageWithSource,
-			Data: &discordgo.InteractionResponseData{
-				Content: "This is the first tester function, good job!",
-			},
-		})
-		if err != nil {
-			fmt.Println("Error with function test-command:")
-			fmt.Println(err)
-		}
-	},
-	"test-command2": func(dg *discordgo.Session, i *discordgo.InteractionCreate) {
-		err := dg.InteractionRespond(i.Interaction, &discordgo.InteractionResponse{
-			Type: discordgo.InteractionResponseChannelMessageWithSource,
-			Data: &discordgo.InteractionResponseData{
-				Content: "This is the second tester function, yay!",
-			},
-		})
-		if err != nil {
-			fmt.Println("Error with function test-command2:")
-			fmt.Println(err)
-		}
-	},
-	"compliment-user": func(dg *discordgo.Session, i *discordgo.InteractionCreate) {
+	"compliment": func(dg *discordgo.Session, i *discordgo.InteractionCreate) {
 		const URI = "https://complimentr.com/api"
 
 		// Access API server to attempt to retrieve a compliment
@@ -103,7 +83,31 @@ var CommandHandlers = map[string]func(dg *discordgo.Session, i *discordgo.Intera
 			fmt.Println("Error with function compliment:")
 			fmt.Println(err)
 		}
-	}}
+	},
+	"bitch": func(dg *discordgo.Session, i *discordgo.InteractionCreate) {
+		var message = ""
+
+		options := i.ApplicationCommandData().Options
+
+		if options[0] != nil && options[0].UserValue(nil).ID != "246732655373189120" {
+			message = "<@" + options[0].UserValue(nil).ID + "> is a bitch."
+		} else if options[0] != nil {
+			message = "<@" + i.Member.User.ID + "> nice try, you're a bitch."
+		}
+
+		err := dg.InteractionRespond(i.Interaction, &discordgo.InteractionResponse{
+			Type: discordgo.InteractionResponseChannelMessageWithSource,
+			Data: &discordgo.InteractionResponseData{
+				Content: message,
+			},
+		})
+
+		if err != nil {
+			fmt.Println("Error with function bitch:")
+			fmt.Println(err)
+		}
+	},
+}
 
 func addHandlers(dg *discordgo.Session, i *discordgo.InteractionCreate) {
 	dg.AddHandler(func(dg *discordgo.Session, i *discordgo.InteractionCreate) {
